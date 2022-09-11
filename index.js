0bdc149355a360aa3f6d7d0495abadac4fa3ce2c
Seth <seth.bbrendle@gmail.com>
2022-09-11 17:31:51 -0400

    commit tree 4b3124517473e51a4f588b3a023416242841b3af

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+/*const dogObjectExample = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+}
+
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(dogObjectExample)
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+.then(r => r.json())
+.then(data => console.log(data.id))
+.catch(function (error) {
+    alert("Bad things!");
+    console.log(error.message);
+})
+*/
+//const formData = 
+
+/*const submitObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+} */
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name, 
+            email
+        })
+    })
+
+    .then(r => r.json())
+    .then(data => document.body.append(data["id"]))
+    .catch((error) => document.body.append(error))    
+}
